df7f0582bcd28132c74eb82463538415a1d1722c
Jeff McKean <jmckean44@gmail.com>
2024-11-06 13:34:08 -0500

    change to subscribe.js

--- src/api/subscribe.js
+++ src/api/subscribe.js
@@ -10,7 +10,7 @@ export default async (req, res) => {
 		const { email, firstName, lastName, phone, hear, broker, comments } = req.body;
 
 		try {
-			const response = await mailchimp.lists.addListMember(process.env.MAILCHIMP_LIST_ID, {
+			const response = await mailchimp.lists.addListMember(import.meta.env.LIST_ID, {
 				email_address: email,
 				status: 'subscribed',
 				merge_fields: {
