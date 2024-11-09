7ac40479a31f2f8fc87d9caa8d609aed8df7c635
Jeff McKean <jmckean44@gmail.com>
2024-11-09 11:10:16 -0500

    /netlify/functions/subscribe.js

--- src/components/FormSF.jsx
+++ src/components/FormSF.jsx
@@ -16,7 +16,7 @@ function RegisterForm() {
 
         console.log(JSON.stringify(data));    
         
-        const response = await fetch('/netlify/functions/subscribe', {
+        const response = await fetch('/.netlify/functions/subscribe', {
           method: 'POST',
           headers: {            
             'Content-Type': 'application/json',                           
